ca38d48b797119cacfdbd6995b64537970c9fbdb
Tomas Tombakas <tomas.tombakas@sendcloud.com>
2020-03-29 16:18:05 +0200

    Rework some navbar styling

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import App from './App.vue';
 import store from './store';
 
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false;
-
 Vue.config.devtools = true
 
 
