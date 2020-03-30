97c556f360bf5bbed8280a7bb128c2b16d715548
Youri <y.h.b.haenen@student.tue.nl>
2020-03-30 10:55:08 +0200

    Categorized data available in navbar for graphs

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,7 +2,11 @@
   <nav>
     <h1>EmergenVis</h1>
     <h5>Dispatch calls</h5>
-    <div v-for="(value, key) in dispatchType" v-bind:key="'dispatch-' + key" class="dispatch-calls-control">
+    <div
+      v-for="(value, key) in dispatchType"
+      v-bind:key="'dispatch-' + key"
+      class="dispatch-calls-control"
+    >
       <input
         type="checkbox"
         :name="value.type"
@@ -13,35 +17,38 @@
       <label
         :for="key"
         style=" -webkit-background-clip: text; margin-left: 10px"
-      >{{  value.type.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); }) }}</label>
+      >{{ value.type.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); }) }}</label>
       <div
         class="service-gradient-block"
         :style="{backgroundImage: 'linear-gradient(to right,' + value.colors + ')'}"
-        >
-      </div>
+      ></div>
     </div>
-   
-      <b-button v-b-toggle.neighborhood-collapse @click="neighborhoodCollapsed=!neighborhoodCollapsed" class="neighborhood-toggle">
-        <h5>
-          Neighborhood
-          <span v-if="!neighborhoodCollapsed">></span>
-          <span v-if="neighborhoodCollapsed" >▼</span>
-        </h5>
-      </b-button>
-      <b-collapse id="neighborhood-collapse" >
-        <div v-for="(value, key) in neighborhood" v-bind:key="'neighborhood-' + key">
-          <input
-            type="checkbox"
-            :name="value.type"
-            :id="key"
-            v-model="value.checked"
-            v-on:change="setAttribute(value.key)"
-            />
-          <label
-            :for="key"
-            style="margin: 2px 10px"
-            >{{ value.type.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); }) }}</label>
-        </div>
+
+    <b-button
+      v-b-toggle.neighborhood-collapse
+      @click="neighborhoodCollapsed=!neighborhoodCollapsed"
+      class="neighborhood-toggle"
+    >
+      <h5>
+        Neighborhood
+        <span v-if="!neighborhoodCollapsed">></span>
+        <span v-if="neighborhoodCollapsed">▼</span>
+      </h5>
+    </b-button>
+    <b-collapse id="neighborhood-collapse">
+      <div v-for="(value, key) in neighborhood" v-bind:key="'neighborhood-' + key">
+        <input
+          type="checkbox"
+          :name="value.type"
+          :id="key"
+          v-model="value.checked"
+          v-on:change="setAttribute(value.key)"
+        />
+        <label
+          :for="key"
+          style="margin: 2px 10px"
+        >{{ value.type.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); }) }}</label>
+      </div>
     </b-collapse>
     <h5>Time period</h5>
     <div class="time-period-toggle">
@@ -75,7 +82,7 @@
 
 <script>
 import axios from "axios";
-import constants from "../constants.js"
+import constants from "../constants.js";
 
 export default {
   computed: {
@@ -86,8 +93,9 @@ export default {
   data() {
     return {
       dispatchType: [
-        { type: "police", 
-          checked: false, 
+        {
+          type: "police",
+          checked: false,
           colors: constants.POLICE_COLORS.join(",")
         },
         {
@@ -130,7 +138,7 @@ export default {
         to: this.timePeriod.to
       };
 
-      this.$store.dispatch('setTimePeriod', this.timePeriod);
+      this.$store.dispatch("setTimePeriod", this.timePeriod);
 
       if (this.timePeriod.from !== null) {
         params.from = this.timePeriod.from;
@@ -155,7 +163,17 @@ export default {
         .then(result => {
           for (let attr in result.data[0]) {
             if (attr.indexOf("_cat") > -1) {
-              continue;
+              if (this.$route.name === "Graphs") {
+                let originalKey = attr.substring(0, attr.indexOf("_cat"));
+                this.neighborhood.push({
+                  type:
+                    this.$store.getters.getCbsKey[originalKey] + " categorized",
+                  key: attr,
+                  checked: false
+                });
+              } else {
+                continue;
+              }
             } else {
               this.neighborhood.push({
                 type: this.$store.getters.getCbsKey[attr],
@@ -174,6 +192,7 @@ export default {
   mounted() {
     this.reloadData();
     this.loadCbsAttributes();
+    console.log(this.$route.name === "Graphs");
   }
 };
 </script>
