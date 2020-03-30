d20bb09489e837aeaf10f6f22c5d31ed56704298
Youri <y.h.b.haenen@student.tue.nl>
2020-03-30 10:46:48 +0200

    Added router to allow for displaying graphs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,41 @@
 import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 import App from './App.vue';
 import store from './store';
 
-Vue.use(BootstrapVue)
+// Views
+import MapView from './components/MapView';
+import Graphs from './components/Graphs';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
-Vue.config.devtools = true
+Vue.config.devtools = true;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Map',
+      component: MapView,
+      props: false
+    },
+    {
+      path: '/graphs',
+      name: 'Graphs',
+      component: Graphs,
+      props: false
+    }
+  ]
+});
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
